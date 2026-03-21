64291a4b81a85a04059f19bc5fa1a35272687d78
Raymond Mukonda <Raymondkogen@gmail.com>
2026-03-21 03:24:05 +0200

    week two worked fixed

---
+++ scripts/weather.js
@@ -0,0 +1,40 @@
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&appid=4958341136d94758a36316c841cd742a';
+
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // testing only
+      displayResults(data); // uncomment when ready
+    } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+apiFetch();
+
+function displayResults(data) {
+
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+
+
+  const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+
+
+  let desc = data.weather[0].description;
+
+
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+
+
+  captionDesc.textContent = `${desc}`;
+}

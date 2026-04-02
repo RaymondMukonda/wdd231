05e69f80af1b890004e3c283183832b1b7ee5dfa
Raymond Mukonda <Raymondkogen@gmail.com>
2026-04-02 16:03:05 +0200

    added css to my thank you and removed the onclick to be used in the js file instead

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,10 +1,19 @@
 // Timestamp
 document.getElementById("timestamp").value = new Date().toISOString();
 
-// Modal logic
+// Modal open logic
 document.querySelectorAll("[data-modal]").forEach(link => {
-  link.addEventListener("click", e => {
-    e.preventDefault();
-    document.getElementById(link.dataset.modal).showModal();
-  });
+    link.addEventListener("click", e => {
+        e.preventDefault();
+        document.getElementById(link.dataset.modal).showModal();
+    });
 });
+
+// close logic
+document.querySelectorAll(".close-btn").forEach(button => {
+    button.addEventListener("click", () => {
+        button.parentElement.close();
+    });
+});
+
+

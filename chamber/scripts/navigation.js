111721055f83c34f1b627d415eb7850803a9913a
Raymond Mukonda <Raymondkogen@gmail.com>
2026-04-02 18:58:52 +0200

    created the discovery page, ensured all my features are working

--- chamber/scripts/navigation.js
+++ chamber/scripts/navigation.js
@@ -4,3 +4,9 @@ const navMenu = document.querySelector("#navMenu");
 menuBtn.addEventListener("click", () => {
 navMenu.classList.toggle("open");
 });
+
+// Set current year for modification
+document.getElementById("year").textContent = new Date().getFullYear();
+
+// Set last modified date
+document.getElementById("lastModified").textContent = document.lastModified;
